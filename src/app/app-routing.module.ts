f1cf11c21bd31f834ff92846dacc13c61d446b40
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-11 23:37:07 +0600

    Homecomponent finall

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PermittedComponent } from './permitted/permitted.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: "home", component: HomeComponent },
@@ -14,6 +15,7 @@ const routes: Routes = [
   {path: "about", component : AboutComponent},
   {path: "contact", component : ContactComponent},
   {path: "login", component : LoginComponent},
+  {path: "dashboard", component : DashboardComponent},
   {path: "organizations", component : OrganizationComponent},
   {path: "securities", component : SecurityOfficerComponent},
   {path: "permitted", component : PermittedComponent}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor() {
+    this.ui()
+   }
 
   ngOnInit(): void {
+  }
+
+  ui(){
+    
+
+      $(".sidebar-dropdown > a").click(function() {
+      $(".sidebar-submenu").slideUp(200);
+      if (
+      $(this)
+        .parent()
+        .hasClass("active")
+      ) {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+        .parent()
+        .removeClass("active");
+      } else {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+        .next(".sidebar-submenu")
+        .slideDown(200);
+      $(this)
+        .parent()
+        .addClass("active");
+      }
+      });
+      
+      $("#close-sidebar").click(function() {
+      $(".page-wrapper").removeClass("toggled");
+      });
+      $("#show-sidebar").click(function() {
+      $(".page-wrapper").addClass("toggled");
+      });
+      
   }
 
 }
