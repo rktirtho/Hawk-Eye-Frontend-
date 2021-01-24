63634fe86ecb8a9cd0f2d5261534ef8b32ab5522
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-24 23:51:16 +0600

    monitoring service added

--- src/app/services/employee-monitoring.service.ts
+++ src/app/services/employee-monitoring.service.ts
@@ -1,9 +1,17 @@
+import { EmployeeMonitoring } from './../model/employee-monitoring';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeMonitoringService {
+  base_url = "http://localhost:8080/api/"
 
-  constructor() { }
+  constructor(private httpClient : HttpClient) { }
+
+  getToday(): Observable<EmployeeMonitoring[]>{
+    return this.httpClient.get<EmployeeMonitoring[]>(`${this.base_url+"monitoring/today"}`);
+  }
 }
