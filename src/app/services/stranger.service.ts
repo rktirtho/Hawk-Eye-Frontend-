fa17356be375d9d874fd09e551869bc5c1ff2f39
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-27 13:13:08 +0600

    stranger class added

--- src/app/services/stranger.service.ts
+++ src/app/services/stranger.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StrangerService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getAllStrangers:Observable<Stran>
 }
