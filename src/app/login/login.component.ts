724c192fb3b917315d6098ab38b7c55694526cce
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-27 21:28:50 +0600

    login navigator done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  username : string ="username"
   
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+   this.router.navigate(["/dashboard"])         
   }
 
 }
--- src/app/model/stranger.ts
+++ src/app/model/stranger.ts
@@ -2,5 +2,5 @@ export class Stranger{
     id : number
     image: string
     visited : number
-    time : string
+    time : Date
 }
